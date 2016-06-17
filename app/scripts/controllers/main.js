897b3e455396bcfb237be977fe0f0c7af6753e9a
Alexey Zerkalenkov <a.zerkalenkov@smart-image.de>
2016-06-17 20:47:40 +0200

    Layout and drag / drop functionality implementation.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,9 @@
  * Controller of the smartbarApp
  */
 angular.module('smartbarApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+  .controller('MainCtrl', function ($scope) {
+
+    $scope.floatMenu = { title: 'Test DRAG' };
+
   });
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -25,6 +25,8 @@ module.exports = function(config) {
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
+      'bower_components/jquery-ui/jquery-ui.js',
+      'bower_components/angular-dragdrop/src/angular-dragdrop.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
