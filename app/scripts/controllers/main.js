7d64d108541c26c2454b067ec1755743b2bd01b5
Alexey Zerkalenkov <a.zerkalenkov@smart-image.de>
2016-06-19 14:26:26 +0200

    Menu fain / out implementation.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,44 +9,56 @@
  */
 angular.module('smartbarApp')
 
-       .controller('MainCtrl', function ($scope, $http, $mdDialog) {
+  .controller('MainCtrl', function ($scope, $http, $mdDialog) {
 
-         $scope.activeMenuIndex = 0;
-         $scope.activeSubmenu = {};
-         $scope.compactMode = false;
+    $scope.activeMenuIndex = 0;
+    $scope.activeSubmenu = {};
+    $scope.compactMode = true;
+    this.hideMenuTID = null;
 
-         var originatorEv;
+    var originatorEv;
 
-         this.openMenu = function ($mdOpenMenu, ev) {
-           originatorEv = ev;
-           $mdOpenMenu(ev);
-         };
+    this.openMenu = function ($mdOpenMenu, ev) {
+      originatorEv = ev;
+      $mdOpenMenu(ev);
+    };
 
-         $http.get('menu.json').then(function (menu) {
-           $scope.floatMenu = menu.data;
-           $scope.activeSubmenu = menu.data[0].items;
-         });
+    $http.get('menu.json').then(function (menu) {
+      $scope.floatMenu = menu.data;
+      $scope.activeSubmenu = menu.data[0].items;
+    });
 
-         this.setActiveSubmenu = function(index, submenu) {
-           $scope.activeMenuIndex = index;
-           $scope.activeSubmenu = submenu.items;
-         };
+    this.setActiveSubmenu = function (index, submenu) {
+      $scope.activeMenuIndex = index;
+      $scope.activeSubmenu = submenu.items;
+    };
 
-         this.toggleSet = function (item) {
-           item.inset = !item.inset;
-         };
-         
-         this.toggleComplactMode = function () {
-           $scope.compactMode = !$scope.compactMode;
-         };
+    this.toggleSet = function (item) {
+      item.inset = !item.inset;
+    };
 
-         this.openDialog = function(name, ev) {
-           $mdDialog.show($mdDialog.alert()
-             .title(name)
-             .textContent('You triggered the "' + name + '" action')
-             .ok('Great')
-             .targetEvent(ev)
-           );
-         };
+    this.toggleComplactMode = function () {
+      $scope.compactMode = !$scope.compactMode;
+    };
 
-       });
+    this.hideFloatMenu = function () {
+      this.hideMenuTID = setTimeout(function () {
+        $('#float-menu').fadeOut();
+      }, 300);
+    };
+
+    this.showFloatMenu = function () {
+      clearTimeout(this.hideMenuTID);
+      $('#float-menu').fadeIn();
+    };
+
+    this.openDialog = function (name, ev) {
+      $mdDialog.show($mdDialog.alert()
+        .title(name)
+        .textContent('You triggered the "' + name + '" action')
+        .ok('Great')
+        .targetEvent(ev)
+      );
+    };
+
+  });
